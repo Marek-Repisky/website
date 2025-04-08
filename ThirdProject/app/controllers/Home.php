5126c3c7d8b323ca7c6df58d4c1371e326199e91
DELL <marek.repisky55313@gmail.com>
2025-04-08 23:52:52 +0200

    Started the work on the model

--- ThirdProject/app/controllers/Home.php
+++ ThirdProject/app/controllers/Home.php
@@ -3,8 +3,14 @@
 class Home extends Controller {
 
     public function index($a='', $b='', $c='') {
-        echo "This is the home controller";
+        $model = new Model;
+        // $arr['id'] = 1;
+        $arr['name'] = "John";
+        $arr['age'] = "45";
 
+        $result = $model->insert($arr);
+        
+        show($result);
         $this->view('home');
     }
 
--- ThirdProject/app/core/config.php
+++ ThirdProject/app/core/config.php
@@ -1,10 +1,21 @@
 <?php
 
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    define('ROOT', 
-    'http://localhost/website/ThirdProject/public/');
+    /** database config **/
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+
+    define('ROOT', 'http://localhost/website/ThirdProject/public/');
 } else {
-    define('ROOT', 
-    'https://www.yourwebsite.com');
-    
+    /** database config **/
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+
+    define('ROOT', 'https://www.yourwebsite.com');
 }
